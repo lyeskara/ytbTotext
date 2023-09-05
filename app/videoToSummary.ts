c345540ab996b1b23584a44410a4ca0fae995b01
lyeskara <lyeskara1106@gmail.com>
2023-09-05 12:00:07 -0400

    tailwind classes added

---
+++ app/videoToSummary.ts
@@ -0,0 +1,123 @@
+import ytdl from 'ytdl-core';
+import fs from 'fs'
+import ffmpeg from 'fluent-ffmpeg'
+import { URL, URLSearchParams } from 'url'
+import { Configuration, OpenAIApi } from "openai"
+import Error from 'next/error';
+import { error } from 'console';
+import { resolve } from 'path';
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+    baseOptions: {
+        maxBodyLength: 26214400 // 25 MB
+    }
+});
+
+const openai = new OpenAIApi(configuration);
+
+let summary: string | undefined;
+
+export async function videoToSummary(urlstring: string) {
+
+    const videoID = getYouTubeVideoID(urlstring)
+    const outputFile = `/home/music/${videoID}.mp3`
+
+    const videoReadableStream = ytdl(urlstring, {
+        filter: 'audioonly',
+        quality: 'highestaudio',
+    });
+
+    await convertVideoToAudio(videoReadableStream, outputFile)
+
+    try {
+        const transcript = await transcribeAudio(outputFile);
+        if (!transcript) {
+            console.log('No transcript available.');
+            return null;
+        }
+        summary = await SummarizeTranscript(transcript)
+        if (!summary) {
+            console.log('No Summary available.');
+            return null;
+        }
+    } catch (error) {
+        console.error('Error processing audio:', error);
+        return null;
+    }
+
+}
+
+function getYouTubeVideoID(url: string) {
+    const urlObj = new URL(url);
+    const params = new URLSearchParams(urlObj.search);
+    return params.get('v');
+};
+
+function convertVideoToAudio(videoReadableStream: any, outputFile: string) {
+    return new Promise((resolve, reject) => {
+        ffmpeg(videoReadableStream)
+            .audioBitrate(128)
+            .toFormat('mp3')
+            .save(outputFile)
+            .on('end', () => {
+                resolve('Conversion from video to audio complete.');
+            })
+            .on('error', (err) => {
+                console.error(err);
+                reject(err);
+            });
+    })
+
+}
+
+async function transcribeAudio(filename: string) {
+    try {
+        const transcript = await openai.createTranscription(
+            fs.createReadStream(filename) as any,
+            "whisper-1"
+        );
+        return transcript.data.text
+    } catch (Error) {
+        // Handle the error 
+        console.error("An error occurred:", Error);
+    }
+}
+
+async function SummarizeTranscript(transcript: any) {
+    try {
+        // Call the API with the current conversation
+        const GPTresponse = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: "user",
+                    content: `i will give a prompt that is speaking of a certain subject.
+                              i want you to include the main points of this subject. 
+                              include what the author said that is important and add details if the author is lacking. 
+                              i would like you to be concise in your answer with no boilerplate just straight to the point.
+                              dont go over 20 lines when explaining. 
+                              use bullet points when it comes to instructions or list of things.
+                              here is the prompt: ${transcript}`
+                }],
+        });
+        // Get Ai text
+        let summary = GPTresponse?.data?.choices[0]?.message?.content
+        if (summary !== undefined) {
+            return summary
+        }
+
+    } catch (Error) {
+        // Handle the error 
+        console.error("An error occurred:", Error);
+
+    }
+}
+
+export function displaySummary() {
+    if (summary) {
+        return summary
+    }
+    return null
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const webpack = require('webpack');
+
+const nextConfig = {
+    experimental: {
+        serverActions: true,
+    },
+    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+        // Note: We provide webpack as an argument to the function
+        // You should not require() webpack outside of this function.
+    
+        // Define an environment variable
+        config.plugins.push(
+          new webpack.DefinePlugin({
+            'process.env.FLUENTFFMPEG_COV': JSON.stringify(false),
+          })
+        );
+    
+        return config;
+      },
+}
+
+ 
+
 
 module.exports = nextConfig
